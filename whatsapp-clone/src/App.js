8d9571233457e1ed9513daf16f1b59f3d5457c25
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2021-06-02 12:49:32 +0530

    Chat header complete

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <div className='app_body'>
         <Sidebar />
+        <Chat />
       </div>
     </div>
   );
