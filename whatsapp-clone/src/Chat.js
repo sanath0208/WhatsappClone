170cdf5b709d85c003cff6aa07498e8f160ced17
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2021-06-07 12:13:24 +0530

    skeleton complete

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,7 +1,8 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import React, {useState, useEffect} from 'react';
 import './Chat.css';
-import {AttachFile, MoreVert, SearchOutlined} from '@material-ui/icons';
+import {AttachFile, InsertEmoticon, MoreVert, SearchOutlined} from '@material-ui/icons';
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
 
@@ -36,10 +37,21 @@ function Chat() {
             </div>
 
             <div className="chat_body">
-            
+                <p className="chat_message">
+                    <span className="chat_name">Sanath</span>
+                    Hey Guys
+                    <span className="chat_timestamp">11:52AM</span>
+                </p>
+
             </div>
 
             <div className="chat_footer">
+                <InsertEmoticon />
+                <form>
+                    <input type="text" />
+                    <button type="submit">Send a Message</button>
+                </form>
+                <MicIcon />
             </div>
 
         </div>
