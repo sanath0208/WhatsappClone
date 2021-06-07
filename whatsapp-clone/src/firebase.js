54c5e24933f87a50509079c41951d2cf51fab477
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2021-06-07 12:38:14 +0530

    setting up firebase

--- whatsapp-clone/src/firebase.js
+++ whatsapp-clone/src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
     apiKey: "AIzaSyBC_25qAm7U_I_hCAwb0CBo2EAYQzkWs0A",
     authDomain: "whatsapp-clone-8cd3c.firebaseapp.com",
@@ -8,3 +9,11 @@ const firebaseConfig = {
     appId: "1:1059954463925:web:9f07f89614105192581afc",
     measurementId: "G-SMTN8RW0F2"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
