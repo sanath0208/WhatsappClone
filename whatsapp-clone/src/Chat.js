d1a778159b3006912705030318d40172430a93d3
Sai Sanath K <53268864+sanath0208@users.noreply.github.com>
2021-06-07 16:19:34 +0530

    Update Chat.js

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -3,11 +3,23 @@ import React, {useState, useEffect} from 'react';
 import './Chat.css';
 import {AttachFile, InsertEmoticon, MoreVert, SearchOutlined} from '@material-ui/icons';
 import MicIcon from "@material-ui/icons/Mic";
+import {useParams} from "react-router-dom";
+import db from './firebase'
 
 function Chat() {
 
     const [seed, setSeed] = useState("");
     const [input, setInput] = useState("");
+    const {roomId} = useParams();
+    const [roomName, setRoomName] = useState("");
+
+    useEffect(() => {
+        if (roomId) {
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
+                setRoomName(snapshot.data().name)
+            ))
+        }
+    }, [roomId]);
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000));
@@ -25,7 +37,7 @@ function Chat() {
             <div className = "chat_header">
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
                 <div className="chat_headerInfo">
-                <h3>Room Name</h3>
+                <h3>{roomName}</h3>
                 <p>Last Seen at ...</p>
                 </div>
 
