5b1172fa833d80ba687162c5ce72353e25d8098e
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2021-06-07 15:20:36 +0530

    Router Setup

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import React, {useState} from 'react';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import { BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import {useStateValue} from './StateProvider';
 
 
 function App() {
   return (
     <div className="App">
       <div className='app_body'>
-        <Sidebar />
-        <Chat />
+      <Router>
+        <Switch>
+          <Sidebar />
+          <Chat />
+        </Switch>
+      </Router>
       </div>
     </div>
   );
