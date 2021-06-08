c243a224d65b257eebd93019f405b2fb17c32b6c
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2021-06-08 15:14:52 +0530

    Login Updated

--- whatsapp-clone/src/Login.js
+++ whatsapp-clone/src/Login.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import "./Login.css";
 import { Button } from '@material-ui/core';
+import {auth, provider} from "./firebase";
 
 function Login() {
     const signIn = () => {
-
+        auth.signInWithPopup(provider).then(result =>
+            console.log(result))
+            .catch((error) => alert(error.message))
     };
     return (
-        <div className="Llogin">
+        <div className="login">
             <div className="login_container">
             <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt=""/> 
             <div className="login_text">
